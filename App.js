b711b16e790c872ea3cffb88d3418f1ac8f15386
McCambley <jakemccamb16@gmail.com>
2024-05-08 08:04:02 -0400

    Add gesture functionality to emoji picker

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import CircleButton from "./components/CircleButton";
 import EmojiPicker from "./components/EmojiPicker";
 import EmojiList from "./components/EmojiList";
 import EmojiSticker from "./components/EmojiSticker";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
@@ -49,7 +50,7 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <Text style={styles.text}>Open up app.js to start working on your app!</Text>
       <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
       {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
@@ -71,7 +72,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
